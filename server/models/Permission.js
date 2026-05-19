1df7114fd35ccb389a9697bc43b364d35a6ac324
Ankush Samanta <ankushsamanta2019@gmail.com>
2026-05-20 00:30:03 +0530

    feat: Add Permission model

---
+++ server/models/Permission.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const permissionSchema = new mongoose.Schema({
+  // Use a singleton document with a known ID to store global permissions
+  singleton: {
+    type: String,
+    default: 'GLOBAL',
+    unique: true
+  },
+  admins: {
+    type: [String],
+    default: []
+  },
+  editors: {
+    type: [String],
+    default: []
+  },
+  conversers: {
+    type: [String],
+    default: []
+  },
+  blocked: {
+    type: [String],
+    default: []
+  }
+});
+
+module.exports = mongoose.model('Permission', permissionSchema);
